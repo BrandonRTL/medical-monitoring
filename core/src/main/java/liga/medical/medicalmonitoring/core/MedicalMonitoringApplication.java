723d719922f6bdf1f1c9c65c101f9a3f38808a1d
чсм чсм <faitneo@gmail.com>
2022-10-15 01:43:59 +0300

    hometask 4

--- core/src/main/java/liga/medical/medicalmonitoring/core/MedicalMonitoringApplication.java
+++ core/src/main/java/liga/medical/medicalmonitoring/core/MedicalMonitoringApplication.java
@@ -3,7 +3,6 @@ package liga.medical.medicalmonitoring.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
 @SpringBootApplication
 @ComponentScan(basePackages = {"liga.medical.medicalmonitoring", "liga.medical.common.service"})
 public class MedicalMonitoringApplication {
